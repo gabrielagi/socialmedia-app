91703a9862a3a787ddd29d2c451ad6278056a8e2
gabrielagi <gabrielagi503@gmail.com>
2023-01-31 07:52:22 -0300

    Install and config Redux

--- App.tsx
+++ App.tsx
@@ -1,26 +1,15 @@
 import { Component } from 'react';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Alert, StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 import SwitchNavigator from './navigation/LoginNavigator'
+import thunkMiddleware from 'redux-thunk'
+import reducer from './reducers/index'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
+
+const middleware = applyMiddleware(thunkMiddleware)
+const store = createStore(reducer, middleware)
 
 export default class App extends React.Component {
-
-  state = {
-    niceNumber: 0
-  }
-
-  adding = () => {
-    this.setState({niceNumber: this.state.niceNumber+1})
-  }
-  
-  substract = () => {
-    this.setState({niceNumber: this.state.niceNumber-1})
-  }
-  
-  setToZero = () => {
-    this.setState({niceNumber: 0})
-  }
 
   render() {
     return (
