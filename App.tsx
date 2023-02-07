274ff473d17ea0ff18ac58d5d540e52bed0d90de
gabrielagi <gabrielagi503@gmail.com>
2023-02-07 18:09:17 -0300

    redux rootReducer / Provider

--- App.tsx
+++ App.tsx
@@ -2,17 +2,22 @@ import { Component } from 'react';
 import React from 'react';
 import SwitchNavigator from './navigation/LoginNavigator'
 import thunkMiddleware from 'redux-thunk'
-import reducer from './reducers/index'
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux'
+//import { applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducers/index'
 
-const middleware = applyMiddleware(thunkMiddleware)
-const store = createStore(reducer, middleware)
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+})
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <SwitchNavigator/>
+      <Provider store={store}>
+        <SwitchNavigator/>
+      </Provider>
     )}
     }
